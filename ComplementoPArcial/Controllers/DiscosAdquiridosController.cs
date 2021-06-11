ee68e2730b2e535d63a28f251dc18c5e22f3ad5c
Julio Canizalez <juliocanizalez@outlook.com>
2021-06-10 20:44:25 -0600

    [ADD] intial files

---
+++ ComplementoPArcial/Controllers/DiscosAdquiridosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComplementoPArcial.Controllers
+{
+    public class DiscosAdquiridosController : Controller
+    {
+        // GET: DiscosAdquiridos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
